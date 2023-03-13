399dcea8565e84dc7e6adfb67e2f360d890571e1
RafaJR <rafael.jimenez.reina@gmail.com>
2023-03-13 12:57:02 +0100

    https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true

---
+++ src/main/java/hackerrank/cases/easy/verybigsum/Solution.java
@@ -0,0 +1,46 @@
+// https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+package hackerrank.cases.easy.verybigsum;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<Long> ar) {
+
+        return ar.parallelStream().mapToLong(n -> Long.valueOf(n)).sum();
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
